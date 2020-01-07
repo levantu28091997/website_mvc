2f9bd3b283c884e831ebb180ef01956ac6b172f6
anh tu <levantu.nd1997@gmail.com>
2020-01-07 16:36:02 +0700

    config folder website

---
+++ helpers/format.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * 
+	 */
+	class Format
+	{
+		
+		function formatDate($date){
+			return date("F j, Y, g:i a", strtotime($date))
+		}
+
+		function textShort($text, $limit=400){
+			$text = $text . " ";
+			$text = substr($text, 0, $limit);
+			$text = substr($text, 0,strrpos($text, ' '));
+			$text = $text . "...";
+			return $text;
+		}
+	}
+?>
